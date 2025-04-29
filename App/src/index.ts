8c4ae447ad62dab365bcce5678f8d1ff1fd88088
Juan <juanandresojeda77@gmail.com>
2025-04-29 15:32:03 -0500

    version 1

---
+++ App/src/index.ts
@@ -0,0 +1,38 @@
+import express from "express";
+import cors from "cors";
+import path from "path";
+import { conexionDbPrincipal } from "./BaseDeDatos/BaseDeDatos.conexion";
+import { PassportStrategy } from "./Autentificacion/Passport.config";
+import usuarioRutas from "./Rutas/usuario.rutas";
+import productoRutas from "./Rutas/productos.rutas";
+import facturasRutas from "./Rutas/facturas.rutas";
+import vendedorRutas from "./Rutas/vendedor.rutas";
+const app = express();
+
+// middlewares necesarios para el servidor
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+app.use(PassportStrategy.initialize());
+app.use("/api/auth", usuarioRutas);
+app.use("/api/product", productoRutas);
+app.use("/api/invoice", facturasRutas);
+app.use("/api/seller", vendedorRutas);
+
+//Renderizar html como estatico en la carpeta public
+app.use(express.static(path.join(__dirname, "../public/frontend/dist")));
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "../public/frontend/dist", "index.html"))
+);
+
+//Configuracion del servidor & puerto
+const server_port = parseInt(process.env.PORT ?? "5000");
+app.listen(server_port, () => {
+  console.log("Servidor esta activo en el puerto:", server_port);
+  conexionDbPrincipal();
+});
+export default app;
